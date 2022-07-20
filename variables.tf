448a7907bcfa596b52aaeb055f47c4890a3aa36e
Rahul M R <rahulmasquee@gmail.com>
2022-07-20 15:01:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -113,7 +113,7 @@ variable "repository_description" {
 }
 
 variable "git_repo" {
-  default = "https://github.com/RahulMR42/oci-sample-function-with-python"
+  default = "https://github.com/oracle-devrel/python-oci-devops-function"
 }
 
 variable "git_repo_name" {
